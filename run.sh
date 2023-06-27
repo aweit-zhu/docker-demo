7d96c2b669855d1cb282660b949223d0b0ea43df
阿偉特 <46418398+aweit-zhu@users.noreply.github.com>
2023-06-27 10:06:02 +0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 group_name='aweit'
 app_name='docker-demo'
-app_version='1.0-SNAPSHOT'
 profile_active='prod'
 
 echo '----copy jar----'
@@ -10,18 +9,18 @@ echo '----stop container----'
 docker rm ${app_name}
 
 echo '----rm container----'
-docker rmi ${group_name}/${app_name}:${app_version}
+docker rmi ${group_name}/${app_name}
 
 echo '----rm image----'
-docker build -t ${group_name}/${app_name}:${app_version} .
+docker build -t ${group_name}/${app_name} .
 
 echo '----push imag----'
 docker login -u aweit -p xx051821xx051821
-docker push ${group_name}/${app_name}:${app_version}
+docker push ${group_name}/${app_name}
 
 echo '----build image----'
 docker run -p 8090:8090 --name ${app_name} \
 -v /mydata/app/${app_name}/logs:/var/logs \
--d ${group_name}/${app_name}:${app_version}
+-d ${group_name}/${app_name}
 
 echo '----start container----'
